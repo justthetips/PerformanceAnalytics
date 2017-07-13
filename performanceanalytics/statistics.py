d9276c1ef9ac03ec707de0642e3e6c164ed8750d
Jacob Bourne <jacob.bourne@gmail.com>
2017-07-13 18:36:22 -0400

    started the stats table

--- performanceanalytics/statistics.py
+++ performanceanalytics/statistics.py
@@ -20,4 +20,25 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import pandas as pd
+import numpy as np
+import scipy as sp
+import scipy.stats
 
+
+def geo_mean(data):
+    """
+    quickly calculate the geometric mean of a series
+    :param iterable: an iterable series
+    :return: the geometric mean
+    """
+    a = np.log(data)
+    return np.exp(a.sum() / len(a))
+
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1 + confidence) / 2., n - 1)
+    return m, m - h, m + h
